b758d7cad06f4abe245a7f19658d393298c1b604
liuxiaodong <csusheep@hotmail.com>
2018-04-13 14:10:16 +0800

    解决bug

--- Source/iOS_OSX/Code/LSCContext.m
+++ Source/iOS_OSX/Code/LSCContext.m
@@ -76,11 +76,11 @@ static NSString *const LSCCacheLuaExceptionHandlerName = @"__catchExcepitonHandl
 
 - (void)dealloc
 {
-    //由于LSCSession在销毁前会进行一次GC，但是在该情况下lua_State已经被close。
-    //因此，解决方法是保留state对象，然后先销毁session，在进行close
-    lua_State *state = self.mainSession.state;
-    self.mainSession = nil;
-    [LSCEngineAdapter close:state];
+//    //由于LSCSession在销毁前会进行一次GC，但是在该情况下lua_State已经被close。
+//    //因此，解决方法是保留state对象，然后先销毁session，在进行close
+//    lua_State *state = self.mainSession.state;
+//    self.mainSession = nil;
+//    [LSCEngineAdapter close:state];
 }
 
 - (LSCSession *)currentSession
